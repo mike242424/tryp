537f629383d43a7917b3de9f0c5dd8e730c1181e
Michael Byers <mikebyers24@gmail.com>
2024-06-25 10:33:33 -0400

    navbar component.

---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import React from 'react';
+import TrypLogo from '@/public/tryp.svg';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="p-6 bg-primary">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src={TrypLogo} alt="Tryp Logo" width={20} height={20} />
+        <h1 className="font-bold text-2xl text-secondary">Tryp</h1>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
